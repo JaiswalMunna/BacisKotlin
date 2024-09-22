167fefcdcb37a51f93e8b953b1b65cf75cd949cb
MunnaJaiswal <codermunna007@gmail.com>
2024-09-23 00:16:40 +0545

    textfield layout

--- app/src/main/java/com/example/basickotlin/MainActivity.kt
+++ app/src/main/java/com/example/basickotlin/MainActivity.kt
@@ -4,18 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()-> it will display the content above the safearea
@@ -68,7 +82,10 @@ class MainActivity : ComponentActivity() {
 //            layout()
 
             //cardLayout
-            CardLayout()
+//            CardLayout()
+
+            //textfield in compose
+            textField()
 
         }
     }
@@ -156,23 +173,46 @@ class MainActivity : ComponentActivity() {
 //    }
 
     //card and image layout
-    @Composable
-    fun CardLayout(){
-        Card(
-            modifier = Modifier.padding(horizontal = 8.dp, vertical = 0.dp),
-            elevation = CardDefaults.cardElevation(10.dp)
-        ) {
-            Column {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = "image of kotlin",
-                    modifier = Modifier.padding(top = 8.dp, bottom = 0.dp)
-                )
-                Text(text = "hi", modifier = Modifier
-                    .padding(10.dp)
-                    .align(Alignment.CenterHorizontally))
-            }
+//    @Composable
+//    fun CardLayout(){
+//        Card(
+//            modifier = Modifier.padding(horizontal = 8.dp, vertical = 0.dp),
+//            elevation = CardDefaults.cardElevation(10.dp)
+//        ) {
+//            Column {
+//                Image(
+//                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+//                    contentDescription = "image of kotlin",
+//                    modifier = Modifier.padding(top = 8.dp, bottom = 0.dp)
+//                )
+//                Text(text = "hi", modifier = Modifier
+//                    .padding(10.dp)
+//                    .align(Alignment.CenterHorizontally))
+//            }
+//
+//        }
+//    }
 
+    //textfield in compose
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun textField(){
+        var valueOfField by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+        Box (contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ){
+            TextField(value =valueOfField, onValueChange = {
+                valueOfField=it
+            },
+                label = {Text(text = "Name:")} ,
+                placeholder = { Text(text = "Enter your name:")},//placeholder
+                leadingIcon = {Icon(Icons.Default.Person,contentDescription=null)},//icon in starting
+                trailingIcon = {Icon(Icons.Default.Info, contentDescription = null)}, //icon in ending
+                singleLine = true, //the text will contain single line
+                )
         }
     }
 
